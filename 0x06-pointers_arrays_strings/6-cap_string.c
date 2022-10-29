ac9986bacafb329cc748f57c0a9f4b044ca4a004
Patrick4Ndolo <ndolopatrick52@gmail.com>
2022-10-29 00:20:50 +0000

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,19 +15,19 @@ char *cap_string(char *str)
 	{
 		while (!(str[k] >= 'a' && str[k] <= 'z'))
 			k++;
-		if (str[k - 1] == ' ' || 
+		if (str[k - 1] == ' ' ||
 		    str[k - 1] == '\t' ||
-		    str[k - 1] == '\n' || 
+		    str[k - 1] == '\n' ||
 		    str[k - 1] == ',' ||
-		    str[k - 1] == ';' || 
+		    str[k - 1] == ';' ||
 		    str[k - 1] == '.' ||
-		    str[k - 1] == '!' || 
+		    str[k - 1] == '!' ||
 		    str[k - 1] == '?' ||
-		    str[k - 1] == '"' || 
+		    str[k - 1] == '"' ||
 		    str[k - 1] == '(' ||
-		    str[k - 1] == ')' || 
+		    str[k - 1] == ')' ||
 		    str[k - 1] == '{' ||
-		    str[k - 1] == '}' || 
+		    str[k - 1] == '}' ||
 		    k == 0)
 			str[k] -= 32;
 		k++;
