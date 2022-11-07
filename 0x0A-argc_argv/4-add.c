7cb1451dfae2a330b2e9fd85be10e4442d00a2fb
Patrick4Ndolo <ndolopatrick52@gmail.com>
2022-11-07 21:15:04 +0000

    0x06-pointers_arrays_strings

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+/**
+  *checking_digit - if digit
+  *@str: - array string
+  *Return: success
+  */
+int checking_digit(char *str)
+{
+	unsigned int counter;
+	counter = 0;
+
+	while (counter < strlen(str))
+	{
+		if (!isdigit(str[counter]))
+		{
+			return (1);
+		}
+
+	}
+	return (0);
+}
 
 /**
   *main - Entry
@@ -9,19 +29,30 @@
   *@argv: - parameter 2
   *Return: - 1 if int, and 0 otherwise
   */
-int add_num(char *str)
+int main(int argc, char *argv[])
 {
-	unsigned int i;
+	int i = 1;
+	int str_int;
+	int sum = 0;
 
-	i = 0;
-
-	while (i < strlen(str))
-		printf("%d\n, )
-
-
-	
+	while (i < argc)
+	{
+		if (checking_digit(argv[i]))
+		{
+			str_int = atoi(argv[i]);
+			sum += str_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	i++;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
+
 
 
 
